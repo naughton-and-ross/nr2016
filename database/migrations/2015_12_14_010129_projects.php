c43f54f467f07edbfdb17cd5c5a0b65f2295e3e1
William Naughton-Gravette <william@willng.me>
2015-12-28 00:09:20 +1100

    project schema v1

---
+++ database/migrations/2015_12_14_010129_projects.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Projects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('project_name');
+            $table->string('project_desc');
+            $table->string('project_catagory');
+            $table->text('project_info');
+            $table->integer('photo_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
