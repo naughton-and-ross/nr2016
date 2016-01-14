1397c1d0289c28d92c374b07706b73d32db74e78
William Naughton-Gravette <william@willng.me>
2016-01-15 01:57:09 +1100

    db spot for slug

---
+++ database/migrations/2016_01_14_144924_add_slug_to_projects.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlugToProjects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('project_slug')->after('project_info');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+             $table->dropColumn('project_slug');
+        });
+    }
+}
