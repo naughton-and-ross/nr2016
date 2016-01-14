9e224a0182cea7778d19511c7f0ecc77bdbb717c
William Naughton-Gravette <william@willng.me>
2016-01-15 01:37:34 +1100

    disable registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,8 +32,8 @@ Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 
 // Admin routes...
