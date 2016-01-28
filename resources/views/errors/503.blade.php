4ec0b7383940426451514a712be745e3575d0461
William Naughton-Gravette <william@willng.me>
2016-01-28 16:50:00 +1100

    update 503 error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,26 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+<html lang="en">
+	<head>
+		<script src="https://use.typekit.net/fob6vhu.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+			<title>V3</title>
+            <link rel="stylesheet" href="{{asset('/css/global.css')}}"/>
+			<link rel="stylesheet" href="{{asset('/css/home.css')}}"/>
+			<link rel="stylesheet" href="{{asset('/css/grid.css')}}"/>
+			<!--[if IE]>
+				<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<![endif]-->
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.css"/>
+			<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.css" rel="stylesheet">
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
-            </div>
-        </div>
-    </body>
+	</head>
+	<body>
+		<div class="error-wrap">
+			<div class="error">
+				<h1>We'll be back in a few seconds.</h1>
+			</div>
+		</div>
+	</body>
 </html>
