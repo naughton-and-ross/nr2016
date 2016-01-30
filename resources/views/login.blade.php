8a8bac7374d0960cff73e25a9b1f44532d86da35
tomross9 <ross.tom.9@gmail.com>
2016-01-30 13:37:07 +1100

    login bg linking

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,7 +16,7 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	</head>
-	<body class="login-bg">
+	<body class="login-bg" style="background-image: url({{asset('img/login_feather.jpg')}});">
 			@include('partials.nav-white')
 			<div class="login-wrap">
 				<div class="login">
